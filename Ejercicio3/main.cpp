0e3d25df21a65e3abc3d160037982e73f4517a56
Jhorman <jhorman.carrillo@udea.edu.co>
2018-02-23 11:52:49 -0500

    Ejercicio 3 con comentarios

--- Ejercicio3/main.cpp
+++ Ejercicio3/main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    int A,B;
+    int A,B; /* se declara las dos variableas a ingresar como enteras */
     cout<<"Por favor ingresar dos números ";
-    cin>> A >>B;
-    if (A>B){
+    cin>> A >>B; /* se hace un input para darle valor a las dos variables enteras */
+    if (A>B){ /* se crea un condicional para verificar si el número A es mayor que B */
         cout<<A<<"es mayor que "<<B<<endl;
     }
-    else{
+    else{ /* si no se cumple el primer IF entonces nos dirá que B es mayor que A por consecuencia */
         cout<<B<< " es mayor que "<<A<<endl;
     }
 }
