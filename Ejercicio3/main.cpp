200bad05b710e8c460942dd858d457ada2ba71cb
Jhorman <jhorman.carrillo@udea.edu.co>
2018-02-23 11:41:15 -0500

    Ejercicio 3 hecho

---
+++ Ejercicio3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B;
+    cout<<"Por favor ingresar dos números ";
+    cin>> A >>B;
+    if (A>B){
+        cout<<A<<"es mayor que "<<B<<endl;
+    }
+    else{
+        cout<<B<< " es mayor que "<<A<<endl;
+    }
+}
