df891c3be462d1513a2d39b5474d63ad3d6a14b4
Jhorman <jhorman.carrillo@udea.edu.co>
2018-02-24 17:17:33 -0500

    Ejercicio12

--- Ejercicio12/main.cpp
+++ Ejercicio12/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main()
+/* Este programa calcula las primeras potencias de un número dado por el usuario*/
+
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int i, exp, numero; /* declaramos las variables como enteros*/
+
+    cout<<"ingrese por favor el número para hallar sus primeras 5 potencias ";
+
+    cin>>numero;/* hacemos un output para pedir al usuario que ingrese el numero*/
+
+    for(i=1; i<=5; i++){ /* realizamos un ciclo para que recorra desde el 1 hasta el 5*/
+    exp=pow(numero,i);/* con la función pow(base,n) siendo base= numero, y n= i*/
+
+    cout<<numero<<"^"<<i<<"="<<exp<<endl; /* mostramos el resultado de forma ordenada,
+                                            ejemplo 1^1=1; 1^2=1 así hasta 5 y con salto de linea*/
+        }
+
 }
