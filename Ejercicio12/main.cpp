01bd4c958ff9bbecb2f2104b34da117701ac2ba3
Jhorman <jhorman.carrillo@udea.edu.co>
2018-02-26 11:31:36 -0500

    Ejercicio 12 hecho

--- Ejercicio12/main.cpp
+++ Ejercicio12/main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
-#include <math.h> /* agregamos este paquete para poder usar la función de pow.*/
-
 using namespace std;
 
 int main()
 /* Este programa calcula las primeras potencias de un número dado por el usuario*/
 
 {
-    int i, exp, numero; /* declaramos las variables como enteros.*/
+    int exp, numero; /* declaramos las variables como enteros.*/
 
     cout<<"ingrese por favor el número para hallar sus primeras 5 potencias ";
 
     cin>>numero;/* hacemos un output para pedir al usuario que ingrese el numero*/
 
-    for(i=1; i<=5; i++){ /* realizamos un ciclo para que recorra desde el 1 hasta el 5*/
-    exp=pow(numero,i);/* con la función pow(base,n) siendo base= numero, y n= i*/
 
-    cout<<numero<<"^"<<i<<"="<<exp<<endl; /* mostramos el resultado de forma ordenada,
-                                            ejemplo 1^1=1; 1^2=1 así hasta 5 y con salto de linea*/
-        }
+  /* Hacemos un procedimiento manual multiplicando hasta las 5 veces para hallar sus potencias con un cout para
+   * cada potencia desde uno hasta 5 */
+
+    exp= numero*1;
+    cout<<numero<<"^"<<"1"<<"="<<exp<<endl;
+    exp= numero*numero;
+    cout<<numero<<"^"<<"2"<<"="<<exp<<endl;
+    exp= numero*numero*numero;
+    cout<<numero<<"^"<<"3"<<"="<<exp<<endl;
+    exp= numero*numero*numero*numero;
+    cout<<numero<<"^"<<"4"<<"="<<exp<<endl;
+    exp= numero*numero*numero*numero*numero;
+    cout<<numero<<"^"<<"5"<<"="<<exp<<endl;
+
 
 }
